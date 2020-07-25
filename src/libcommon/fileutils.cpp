aaf405c806a635c256e54c67827ec545af5e1f94
Samuel Albrecht <elsamuko@gmail.com>
2020-07-25 12:30:25 +0200

    chg: more efficient I/O

--- src/libcommon/fileutils.cpp
+++ src/libcommon/fileutils.cpp
@@ -8,7 +8,15 @@ bool libcommon::fileutils::fileExists( const std::string& filename ) {
 
 std::string libcommon::fileutils::fromFile( const std::string& filename ) {
     std::ifstream file( filename.c_str(), std::ios_base::in | std::ios_base::binary );
-    return std::string( ( std::istreambuf_iterator<char>( file ) ), ( std::istreambuf_iterator<char>() ) );
+
+    file.seekg( 0, std::ios::end );
+    size_t size = ( size_t ) file.tellg();
+    file.seekg( 0, std::ios::beg );
+
+    std::string buffer( size, '\0' );
+    file.read( buffer.data(), buffer.size() );
+
+    return buffer;
 }
 
 
@@ -16,7 +24,7 @@ bool libcommon::fileutils::toFile( const std::string& filename, const std::strin
     std::ofstream file( filename.c_str(), std::ios_base::out | std::ios_base::binary );
 
     if( file.is_open() ) {
-        file << data;
+        file.write( data.data(), data.size() );
         return true;
     }
 
